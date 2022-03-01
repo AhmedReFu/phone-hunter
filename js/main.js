2abd07870e40973028094540b69e00bb57d6cded
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-03-01 22:55:50 +0600

    last part product details

--- js/main.js
+++ js/main.js
@@ -64,3 +64,58 @@ const displayProducts = (productAll) => {
         })
     }
 };
+// get product id dynamic url function
+const getId = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.data))
+
+};
+
+
+// display product details function
+const displayDetails = (product) => {
+    const detailsDiv = document.getElementById('show-details');
+    detailsDiv.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add("row", "shadow")
+    div.innerHTML = `
+    <div class="p-3 col-12  col-lg-6 ">
+        <h3 class="card-text">${product.name} </h3>
+        <h5 class="text-danger">${product.releaseDate ? product.releaseDate : 'release date not found'}</h5>
+        <h5 class="card-text"> Brand: ${product.brand} </h5>
+        <img src="${product.image}" class="card-img-top" alt=""/>
+    </div>
+    <div class=" p-3 col-12 col-lg-6 ">
+         <ul class="list-group">
+            <h5 class="text-center text-info">Main Features</h5>
+            <li class="list-group-item"><h6 class="card-text">ChipSet: ${product.mainFeatures.chipSet} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">Display: ${product.mainFeatures.displaySize} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">Memory: ${product.mainFeatures.memory} </h6></li>
+         </ul>
+         
+         <ul class="list-group">
+            <h5 class="text-center text-info">Sensor Information</h5>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[0]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[1]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[2]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[3]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[4]} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">${product.mainFeatures.sensors[5]} </h6></li>
+         </ul>
+
+         <ul class="list-group">
+            <h5 class="text-center text-info"> Others Information </h5>
+            <li class="list-group-item"><h6 class="card-text">Bluetooth: ${product.others?.Bluetooth} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">GPS: ${product.others?.GPS} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">NFC: ${product.others?.NFC} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">Radio: ${product.others?.Radio} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">USB: ${product.others?.USB} </h6></li>
+            <li class="list-group-item"><h6 class="card-text">WLAN: ${product.others?.WLAN} </h6></li>
+         </ul>
+    </div>
+    `;
+    detailsDiv.appendChild(div);
+};
